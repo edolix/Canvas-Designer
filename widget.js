35cec6a544c285e21c18d9749c34be2db51f80ae
Edoardo Gallo <edo91.gallo@gmail.com>
2018-07-02 13:41:26 +0200

    Include webrtcHandle

--- widget.js
+++ widget.js
@@ -1,4 +1,4 @@
-// Last time updated: 2018-06-04 4:08:16 PM UTC
+// Last time updated: 2018-07-02 11:40:41 AM UTC
 
 // _______________
 // Canvas-Designer
@@ -3670,5 +3670,66 @@
             uid: uid
         }, '*');
     }
+
+    var webrtcHandler = {
+        createOffer: function(callback) {
+            var captureStream = document.getElementById('main-canvas').captureStream(15);
+
+            var peer = this.getPeer();
+            peer.addStream(captureStream);
+            peer.onicecandidate = function(event) {
+                if (!event || !!event.candidate) {
+                    return;
+                }
+
+                callback({
+                    sdp: peer.localDescription.sdp,
+                    type: peer.localDescription.type
+                });
+            };
+            peer.createOffer(function(sdp) {
+                peer.setLocalDescription(sdp);
+            }, function() {}, {
+                mandatory: {
+                    OfferToReceiveAudio: false,
+                    OfferToReceiveVideo: false
+                }
+            });
+        },
+        setRemoteDescription: function(sdp) {
+            this.peer.setRemoteDescription(new RTCSessionDescription(sdp));
+        },
+        createAnswer: function(sdp, callback) {
+            var peer = this.getPeer();
+            this.setRemoteDescription(sdp);
+            peer.onicecandidate = function(event) {
+                if (!event || !!event.candidate) {
+                    return;
+                }
+
+                callback({
+                    sdp: peer.localDescription.sdp,
+                    type: peer.localDescription.type
+                });
+            };
+            peer.createAnswer(function(sdp) {
+                peer.setLocalDescription(sdp);
+            }, function() {}, {
+                mandatory: {
+                    OfferToReceiveAudio: false,
+                    OfferToReceiveVideo: true
+                }
+            });
+            peer.onaddstream = function(event) {
+                callback(event);
+            };
+        },
+        getPeer: function() {
+            var WebRTC_Native_Peer = window.RTCPeerConnection || window.webkitRTCPeerConnection || window.mozRTCPeerConnection;
+            var peer = new WebRTC_Native_Peer(null);
+            this.peer = peer;
+            return peer;
+        }
+    };
 
 })();
